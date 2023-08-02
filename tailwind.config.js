5fe604bf860e0cbe1d7fd2315ae3b60fdd35654d
Dongho Kim <dongre2527@gmail.com>
2023-08-02 12:57:33 +0900

    chore: tailwind.config.js에 theme 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,31 @@ module.exports = {
 		'./<custom directory>/**/*.{js,jsx,ts,tsx}',
 	],
 	theme: {
-		extend: {},
 		fontSize: {
 			sm: ['14px', '20px'],
 			md: ['16px', '24px'],
 			lg: ['20px', '28px'],
 			xl: ['24px', '32px'],
-			
+		},
+		colors: {
+			primary: '#538EF4',
+			secondary: '#EAF6FF',
+			black: '#000000',
+			white: '#FFFFFF',
+			grey: {
+				2: '#C4C3C4',
+				3: '#F7F7FE',
+				4: '#929292',
+				5: '#9C9C9C',
+			},
+		},
+		fontWeight: {
+			700: 700,
+		},
+		borderRadius: {
+			0: 0,
+			8: 8,
+			10: 10,
 		},
 	},
 	plugins: [],
