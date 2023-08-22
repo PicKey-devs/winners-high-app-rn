2f97bdbb2bb50d3a66b0e142a1a7ddc455b6cd56
Dongho Kim <dongre2527@gmail.com>
2023-08-22 13:51:20 +0900

    feat: Text 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { IndexStacks } from './navigations/IndexTabs';
-import React, { useEffect } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import React, { useCallback } from 'react';
 import { View } from 'react-native';
+
+import { IndexStacks } from './navigations/IndexTabs';
+import fonts from '../assets/fonts';
+
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+	const [fontsLoaded, fontError] = useFonts(fonts);
+
+	const onLayoutRootView = useCallback(async () => {
+		if (fontsLoaded || fontError) {
+			await SplashScreen.hideAsync();
+		}
+	}, [fontsLoaded, fontError]);
+
+	if (!fontsLoaded && !fontError) {
+		return null;
+	}
+
 	return (
-		<NavigationContainer>
-			<IndexStacks />
-		</NavigationContainer>
+		<View className="flex-1" onLayout={onLayoutRootView}>
+			<NavigationContainer>
+				<IndexStacks />
+			</NavigationContainer>
+		</View>
 	);
 }
